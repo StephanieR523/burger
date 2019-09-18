b606aad061b46ec72b6e8c3b66498bc1166b104b
Stephanie Romanowski <stephanie.romanowski@vantage-cg.com>
2019-09-18 11:22:59 -0400

    everything

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO burgers (burger_name) VALUES ("Bacon Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Single Deluxe");
+INSERT INTO burgers (burger_name) VALUES ("Double Deluxe");
+INSERT INTO burgers (burger_name) VALUES ("Kids Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Avacado Bacon Burger");
